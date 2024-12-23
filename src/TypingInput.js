c8293df665d94367ab957c64784b75389139b283
qpeterp <lsepeter0827@naver.com>
2024-12-23 15:22:49 +0900

    Fix :: Calculate typing speed

--- src/TypingInput.js
+++ src/TypingInput.js
@@ -39,14 +39,28 @@ function TypingInput() {
     if (userInput.length === codeToType.length) {
       const endTime = new Date().getTime();
       const takenTime = (endTime - startTime) / 1000;
-      const correctChars = userInput
+
+      // 자소 단위로 정확도 및 타수 계산
+      const correctJamoCount = userInput
         .split("")
-        .filter((char, index) => char === codeToType[index]).length;
-      setAccuracy(((correctChars / codeToType.length) * 100).toFixed(1));
-      setSpeed((userInput.length * (100 / takenTime)).toFixed(0));
+        .reduce((count, char, index) => {
+          const targetJamo = splitHangulToJamo(codeToType[index]);
+          console.log(targetJamo);
+          const inputJamo = splitHangulToJamo(char);
+          return (
+            count + targetJamo.filter((jamo, i) => jamo === inputJamo[i]).length
+          );
+        }, 0);
+
+      const totalJamo = countJamo(codeToType);
+      console.log(totalJamo);
+      const inputJamo = countJamo(userInput);
+
+      setAccuracy(((correctJamoCount / totalJamo) * 100).toFixed(1));
+      setSpeed(((inputJamo * 60) / takenTime).toFixed(1));
 
       setIsFinish(true);
-      setIsDialogOpen(true); // 타자 종료 시 dialog 열기
+      setIsDialogOpen(true);
     }
   }, [userInput, codeToType, startTime]);
 
@@ -120,6 +134,108 @@ function TypingInput() {
       )}
     </>
   );
+}
+
+function splitHangulToJamo(char) {
+  const HANGUL_START = 0xac00;
+  const HANGUL_END = 0xd7a3;
+
+  const CHO = [
+    "ㄱ",
+    "ㄲ",
+    "ㄴ",
+    "ㄷ",
+    "ㄸ",
+    "ㄹ",
+    "ㅁ",
+    "ㅂ",
+    "ㅃ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅉ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+  ];
+  const JUNG = [
+    "ㅏ",
+    "ㅐ",
+    "ㅑ",
+    "ㅒ",
+    "ㅓ",
+    "ㅔ",
+    "ㅕ",
+    "ㅖ",
+    "ㅗ",
+    "ㅘ",
+    "ㅙ",
+    "ㅚ",
+    "ㅛ",
+    "ㅜ",
+    "ㅝ",
+    "ㅞ",
+    "ㅟ",
+    "ㅠ",
+    "ㅡ",
+    "ㅢ",
+    "ㅣ",
+  ];
+  const JONG = [
+    "",
+    "ㄱ",
+    "ㄲ",
+    "ㄳ",
+    "ㄴ",
+    "ㄵ",
+    "ㄶ",
+    "ㄷ",
+    "ㄹ",
+    "ㄺ",
+    "ㄻ",
+    "ㄼ",
+    "ㄽ",
+    "ㄾ",
+    "ㄿ",
+    "ㅀ",
+    "ㅁ",
+    "ㅂ",
+    "ㅄ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+  ];
+
+  // 한글 범위 내에 있는 문자일 경우
+  if (
+    char >= String.fromCharCode(HANGUL_START) &&
+    char <= String.fromCharCode(HANGUL_END)
+  ) {
+    const code = char.charCodeAt(0) - HANGUL_START;
+    const cho = Math.floor(code / (21 * 28)); // 초성
+    const jung = Math.floor((code % (21 * 28)) / 28); // 중성
+    const jong = code % 28; // 종성
+
+    return [CHO[cho], JUNG[jung], JONG[jong]].filter(Boolean); // 종성이 없으면 제외
+  }
+
+  // 한글이 아닌 경우 그대로 반환
+  return [char];
+}
+
+function countJamo(text) {
+  return text
+    .split("")
+    .reduce((count, char) => count + splitHangulToJamo(char).length, 0);
 }
 
 export default TypingInput;
