5672faa92041f85d393eac7fae1cf56fefefea35
qpeterp <lsepeter0827@naver.com>
2025-01-08 15:16:24 +0900

    Feat :: Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSUbpAhxK2GwvH6SyroRHlw8EOhTTEH8s",
+  authDomain: "modeogta-b54db.firebaseapp.com",
+  projectId: "modeogta-b54db",
+  storageBucket: "modeogta-b54db.appspot.com",
+  messagingSenderId: "524539897894",
+  appId: "1:524539897894:web:205f85b0e23d29275edee7",
+  measurementId: "G-XG4QH2B2F6",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
