23b53d93e7343c3e23a3d793f83231977c9151e3
qpeterp <lsepeter0827@naver.com>
2025-03-04 17:50:24 +0900

    Fix :: Background change

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -4,9 +4,7 @@ const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
   const [themeColor, setThemeColor] = useState("off");
-  const [backgroundUrl, setBackgroundUrl] = useState(
-    "https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-  );
+  const [backgroundUrl, setBackgroundUrl] = useState("");
 
   useEffect(() => {
     const root = document.documentElement;
